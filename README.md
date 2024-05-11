489b0277f94095d5f52c7fa5dc2956a122acb5d4
Sayed (Tashfi) Nowroz <tnowroz@gmail.com>
2024-05-10 22:40:57 -0400

    Readme Updated

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # sPlotter-Serial-Data-Visualizer
-This is a personalized Serial Data Plotter / Visualizer, in attempt to have something better than what Arduino IDE offers.
+This is a personalized Serial Data Plotter / Visualizer, in attempt to have something better than what Arduino IDE offers. 
+
+![alt text](./assets/UI_overview.png)
--- obj/Debug/net8.0-windows/sPlotter.AssemblyInfo.cs
+++ obj/Debug/net8.0-windows/sPlotter.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("sPlotter")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd87645fc60b8abb061019c4953730789a8de4fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b4755a4f14d089e8518df2bfa99d14c8f9a8904")]
 [assembly: System.Reflection.AssemblyProductAttribute("sPlotter")]
 [assembly: System.Reflection.AssemblyTitleAttribute("sPlotter")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
